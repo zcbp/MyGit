04e2e129868939f9cd7e9cfea9d47ebc0817fea6
liu <3125697626@qq.com>
2018-09-29 18:24:23 +0800

    真的解决了

--- MyGit/src/MyGits/Git.java
+++ MyGit/src/MyGits/Git.java
@@ -1,11 +1,9 @@
 package MyGits;
 
 public class Git {
-<<<<<<< HEAD
+
   //888+999999
-=======
-  //999999
->>>>>>> branch 'master' of https://github.com/zcbp/MyGit.git
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 //1111
